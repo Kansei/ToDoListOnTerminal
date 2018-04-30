cbcbc8a774489a51d857cc434419889910387155
Kansei Kogami <k_kansei_k@yahoo.co.jp>
2018-04-30 13:43:40 +0900

    done: todo

--- todo.rb
+++ todo.rb
@@ -1,4 +1,70 @@
+load_path=File.expand_path("../",__FILE__)
+require "#{load_path}/todo_command"
 
-list_path = File.expand_path("../",__FILE__)
-todo_list = list_path + "todo_list.txt"
-done_list = list_path + "done_list.txt"
+def check_argv
+  file = $0.split("/")[-1]
+  com = file.split(".")[0]
+  if ARGV.size < 2
+    STDERR.print "Usage: #{com} add/delete/check/done\n"
+    exit
+  end
+end
+
+def option_nil?(opt)
+  if opt.nil?
+    STDERR.print "Option is not enough.\n"
+    exit
+  end
+end
+
+def is_it_number?(number)
+  if !(number =~ /^[1-9]+$/)
+    STDERR.print "Please enter a number.\n"
+    exit
+  end
+end
+
+todo_list = load_path + "/"+"todo_list.txt"
+done_list = load_path + "/"+"done_list.txt"
+
+command = ARGV[0]
+
+case command
+when "add"
+  option_nil?(ARGV[1])
+  new_task = ARGV[1]
+
+  add(new_task,todo_list)
+
+  puts "added: #{new_task}"
+
+  check("check",todo_list)
+when "delete"
+  option_nil?(ARGV[1])
+  is_it_number?(ARGV[1])
+
+  task_num = ARGV[1].to_i
+
+  delete_task= delete(task_num,todo_list)
+
+  puts "deleted: #{delete_task}"
+
+  check("check",todo_list)
+when "check"
+  check(command,todo_list)
+when "done"
+  option_nil?(ARGV[1])
+  opt = ARGV[1]
+
+  if opt == "list"
+    check(command, done_list)
+  else
+    is_it_number?(opt)
+    task_num = ARGV[1].to_i
+    done_task = done(task_num, todo_list, done_list)
+
+    puts "done: #{done_task}"
+
+    check(command, done_list)
+  end
+end
--- todo_command.rb
+++ todo_command.rb
@@ -1,15 +1,3 @@
-#listの書き込み、削除、Done、確認
-#
-# todo add "jsndcjn"
-#
-# todo del タスク番号
-#
-# todo check
-#
-# todo done タスク番号
-#
-# todo done list
-
 # タスクの追加
 def add(new_task,list)
   File.open(list, "a") do |f|
@@ -18,17 +6,38 @@ def add(new_task,list)
 end
 
 # タスクの削除
-def delete(tsak_num,todo_list)
+def delete(task_num,todo_list)
   delete_task = ""
+  todo_array = []
 
-  File.open(todo_list, "w+") do |f|
-    todo_array = f.readlines
+  File.open(todo_list, "r") do |file|
+
+    todo_array = file.readlines
+
     delete_task = todo_array[task_num-1]
+
+    if delete_task.nil?
+      STDERR.print "The task does not exist\n"
+      file.close
+      exit
+    end
+
     todo_array.delete_at(task_num-1)
-    todo_array.each{|todo| f.print(todo)}
+  end
+
+  File.open(todo_list, "w") do |file|
+    todo_array.each{|todo| file.print(todo)}
   end
 
   return delete_task
+end
+
+# todoからdoneへのタスクの移動
+def done(task_num,todo_list,done_list)
+  done_task = delete(task_num,todo_list)
+  add(done_task,done_list)
+
+  return done_task
 end
 
 # リストの確認
@@ -45,15 +54,14 @@ def check(comm,list)
   print list_name
   puts "<><><><><><><>"
 
+  i = 1
   File.open(list, "r") do |f|
     tasks = f.readlines
-    tasks.each{|task| print(task)}
+    tasks.each do |task|
+      print("   #{i}  #{task}")
+      i += 1
+    end
   end
 
   puts "<><><><><><><><><><><><><><><><><><><><><>"
-end
-
-def done(task_num,todo_list,done_list)
-  done_task = delete(task_num,todo_list)
-  add(done_task,done_list)
 end
